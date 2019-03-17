fe7597ccaaa3467998a9af964b74e0a6c4162f1e
Tanay Raul <tanay.raul@somaiya.edu>
2019-03-17 21:01:27 +0530

    added db connection template

---
+++ includes/db_conn.php
@@ -0,0 +1,16 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "result_analysis";
+
+try {
+	$conn = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully"; 
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include 'includes/header.html';
+?>
+<h1>Hello world!!</h1>
+
+
+<?php
+phpinfo();
+include 'includes/footer.html';
+?>
