e2894dd813a06659aa9f4434fbdcbc8665bdab8f
murtaza98 <m.patrawala@somaiya.edu>
2019-03-22 19:15:34 +0530

    CHECKPOINT -- student semester filter complete

--- prototype/student_semester.php
+++ prototype/student_semester.php
@@ -3,14 +3,17 @@
 <?php include '../includes/charts_json_wrapper.php' ?>
 <?php
 	if(isset($_GET['sem'])){
-		$roll_no = $_SESSION['uname'];
+
+		$roll_no = substr($_SESSION['uname'],1);  //After getting current sem data from database remove substring fn.
 		$sem_no = $_GET['sem'];
-		// $sem_no = 1;
-		// $seat_no[0] = $sem_no;
-		$sql = "SELECT course_name, ca_marks FROM student_theory_marks NATURAL JOIN courses WHERE seat_no = $seat_no AND semester_no = $sem_no;";
+
+		$seat_no = (string)$sem_no.(string)$roll_no;
+		$sql = "SELECT course_id, ca_marks FROM student_theory_marks WHERE seat_no = $seat_no;";
+		// echo $sql;
+		// $sql = "SELECT course_name, ca_marks FROM student_theory_marks NATURAL JOIN courses WHERE seat_no = $seat_no AND semester_no = $sem_no;";
 		$result = $conn->query($sql);
 		$result_array = $result->fetchAll(PDO::FETCH_ASSOC);	
-		$metadata = array(array("Course Name", "course_name", "string"), array("CA marks", "ca_marks", "number"));
+		$metadata = array(array("Course Name", "course_id", "string"), array("CA marks", "ca_marks", "number"));
 		$string = returnJSONString($result_array, $metadata);
 		echo $string;
 	}
