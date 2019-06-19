48849e066b25bc230db749e8fa457a7a07558219
murtaza98 <m.patrawala@somaiya.edu>
2019-06-19 11:58:37 +0530

    Integrated Login module

--- login_check.php
+++ login_check.php
@@ -1,22 +1,55 @@
 <?php
+include 'includes/db_conn.php';
 include 'includes/header.html';
 session_start();
 
-$uname = $_POST['username'];
-$pwd = $_POST['password'];
 
-$_SESSION['uname'] = $uname;
-$_SESSION['user_type'] = $pwd;
+if(isset($_POST['submit'])){
+	$uname = $_POST['username'];
+	$pwd = $_POST['password'];
 
+	$query = $conn->prepare("SELECT * FROM users WHERE id = ?;");
 
-// Temporary code
-if($uname == "comps_hod"){
-	$_SESSION['hod_department'] = "comps";
+	$status = $query->execute(array($uname));
+
+	if(!$status){
+		echo $status;
+		 die("FAILED" . mysqli_error($conn));
+	}
+
+	$result_array = $query->fetchAll(PDO::FETCH_ASSOC);
+	
+	$id = $result_array[0]['id'];
+	$hash_password = $result_array[0]['password']; 
+	$role = $result_array[0]['role'];
+
+	// verify password
+	if(password_verify($pwd, $hash_password)){
+		// Password verification success
+		$_SESSION['uname'] = $uname;
+
+		// check if role is hod, and set $_SESSION['hod_department'] accordingly
+		// eg:- hod_comps, then in $_SESSION['hod_department'] 'comps' will get stored
+		if (strpos($role, 'hod') !== false) {
+		    $_SESSION['user_type'] = 'hod';
+
+		    $splitted_role = explode("_", $role);
+		    $hod_department = $splitted_role[1];
+
+		    $_SESSION['hod_department'] = $hod_department;
+ 		}else{
+ 			$_SESSION['user_type'] = $role;
+ 		}
+
+ 		header("Location: index.php");
+	}else{
+		// Password verification failed
+		$_SESSION['password_incorrect'] = true;
+		header("Location: login.php");
+	}
 }
 
-header("Location: index.php");
-
-include 'includes/footer.html';
+include 'includes/footer.php';
 ?>
 
 
